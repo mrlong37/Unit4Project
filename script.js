a6bc62afc745625f3dbc8f1db5916586a16e3f11
mrlong37 <37mrlong@gmail.com>
2025-06-16 15:34:24 -0500

    worked on project

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // JavaScript goes here.
+let form = document.getElementById("quizForm");
+let resultDiv = document.getElementById("result");
+let resultImage = document.getAnimations("resultImage");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let ajfoajoife = 0;
+    let questions = ["q1", "q2", "q3", "q4"];
+
+    
+
+
+})
