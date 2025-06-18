f64f6da16bc973d5cd49d3bc9b2b31917b349c57
mrlong37 <37mrlong@gmail.com>
2025-06-18 03:10:15 -0500

    inserted and modified variables

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // JavaScript goes here.
-let form = document.getElementById("quizForm");
-let resultDiv = document.getElementById("result");
-let resultImage = document.getAnimations("resultImage");
+document.getElementById("form1").onsubmit=function() {
+  
+  
+      height = parseInt(document.querySelector('input[name = "q1"]:checked').value);
+  
+      desc = parseInt(document.querySelector('input[name = "q2"]:checked').value);
+  
+      concept = parseInt(document.querySelector('input[name = "q3"]:checked').value);
 
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    let ajfoajoife = 0;
-    let questions = ["q1", "q2", "q3", "q4"];
-
-    
-
-
-})
+      glob = parseInt(document.querySelector('input[name = "q4"]:checked').value);
+  
+      total= height + desc + concept + glob;
+  
+  
+       document.getElementById("answer").innerHTML = total;
+  
+       if(total < 4) {
+  document.getElementById("answer2").innerHTML = "You got Quiet Company.";
+  }
+  
+     if(total >=4 && total < 7) {
+  document.getElementById("answer2").innerHTML = "You got Spoon.";
+  }
+  
+    if(total >=7) {
+  document.getElementById("answer2").innerHTML = "You got Willie Nelson.";
+  }
+  
+       return false; // required to not refresh the page; just leave this here
+       }// end the submit function
